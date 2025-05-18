a877a110959ad195a92bd7b323d6d269b81d573a
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-05-18 14:03:30 +0300

    Убрал Main из пакетов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package Main;
-
 import Enums.Status;
 import TaskManager.TaskManager;
 import Tasks.EpicTask;
@@ -18,21 +16,13 @@ public class Main {
         SubTask subTask1 = new SubTask(epicTask1.getId());
         SubTask subTask2 = new SubTask(epicTask1.getId());
 
-
-//        EpicTask epicTask2 = new EpicTask();
-//        SubTask subTask3 = new SubTask(epicTask2);
-
-//        taskManager.createTask(task1);
-//        taskManager.createTask(task2);
         taskManager.createTask(epicTask1);
         taskManager.createTask(subTask1);
         taskManager.createTask(subTask2);
 
         taskManager.updateTask(subTask1, Status.DONE);
         taskManager.updateTask(subTask2, Status.IN_PROGRESS);
-//        taskManager.createTask(epicTask2);
 
-//        taskManager.deleteTaskByUUID(epicTask1.getId());
         System.out.println(epicTask1.getStatus());
 
     }
