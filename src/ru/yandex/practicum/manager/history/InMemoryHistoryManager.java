676139bf91131cfd7595f1fe60711deb11dd1022
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-07-06 21:59:37 +0300

    Реализована дополнительная логика из интерфейса HistoryManager в классе InMemoryHistoryManager

--- src/ru/yandex/practicum/manager/history/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/manager/history/InMemoryHistoryManager.java
@@ -1,31 +1,50 @@
 package ru.yandex.practicum.manager.history;
 
+import ru.yandex.practicum.manager.history.custom.list.CustomTaskLinkedList;
+import ru.yandex.practicum.manager.history.custom.list.CustomTaskList;
+import ru.yandex.practicum.model.EpicTask;
+import ru.yandex.practicum.model.SubTask;
 import ru.yandex.practicum.model.Task;
 
 import java.util.*;
 
 
-public class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> taskHistory = new ArrayList<>();
-    private static final int HISTORY_LIMIT = 10;
+public class InMemoryHistoryManager <T extends Task> implements HistoryManager<T> {
+    private final CustomTaskList<Task> taskHistory = new CustomTaskLinkedList();
 
     @Override
-    public void add(Task task) {
-        this.checkHistoryList(task);
+    public void add(T task) {
+        this.addTaskCopy(task);
     }
 
     @Override
-    public Collection<Task> getHistory() {
-        return this.taskHistory;
-    }
-
-    private void checkHistoryList(Task task) {
-        Task taskCopy = new Task(task);
-        int size = this.taskHistory.size();
-        if (size == HISTORY_LIMIT) {
-            this.taskHistory.removeFirst();
+    public ArrayList<T> getHistory() {
+        ArrayList<T> history = new ArrayList<>();
+        for (Task task : this.taskHistory) {
+            history.add((T) task);
         }
-        this.taskHistory.add(taskCopy);
+        return history;
+    }
+
+    @Override
+    public void remove(UUID uuid) {
+        this.taskHistory.remove(uuid);
+    }
+
+    @Override
+    public void update(T task) {
+        this.taskHistory.update(task);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void addTaskCopy(T task) {
+        if (task instanceof EpicTask) {
+            taskHistory.add((T) new EpicTask((EpicTask) task));
+        } else if (task instanceof SubTask) {
+            taskHistory.add((T) new SubTask((SubTask) task));
+        } else {
+            taskHistory.add((T) new Task(task));
+        }
     }
 
 }
