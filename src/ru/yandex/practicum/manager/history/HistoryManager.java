e29ccf202b5bece429cf8043bbd108a35b3d888a
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-07-06 21:57:32 +0300

    Добавлена дополнительная логика в HistoryManager

--- src/ru/yandex/practicum/manager/history/HistoryManager.java
+++ src/ru/yandex/practicum/manager/history/HistoryManager.java
@@ -2,9 +2,13 @@ package ru.yandex.practicum.manager.history;
 
 import ru.yandex.practicum.model.Task;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.UUID;
 
-public interface HistoryManager {
-    void add(Task task);
-    Collection<Task> getHistory();
+public interface HistoryManager <T extends Task> {
+    void add(T task);
+    ArrayList<T> getHistory();
+    void remove(UUID uuid);
+    void update(T task);
 }
