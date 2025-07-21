db5b6527c07265d6c67c3deb2cef2903f1f1866d
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-07-21 12:59:06 +0300

    Добавлен класс и интерфейс для сохранения в файл

--- src/ru/yandex/practicum/manager/saver/FileSaver.java
+++ src/ru/yandex/practicum/manager/saver/FileSaver.java
@@ -1,21 +1,25 @@
-package ru.yandex.practicum.manager.filesaver;
+package ru.yandex.practicum.manager.saver;
 
+import ru.yandex.practicum.manager.exception.ManagerSaveException;
 import ru.yandex.practicum.model.Task;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 
 public class FileSaver<T extends Task> implements Saver<T> {
-    private static final String FILE_NAME = "result.txt";
+    private static final String FILE_HEADER = "id, type, name, description, status, epicId/SubtaskIdsList";
 
     @Override
-    public void save(Collection<T> data) {
-        try (BufferedWriter bf = new BufferedWriter(new FileWriter(FILE_NAME,false))) {
+    public void save(Collection<T> data, Path path) {
+        try (BufferedWriter bf = new BufferedWriter(Files.newBufferedWriter(path))) {
+            bf.write(FILE_HEADER + System.lineSeparator());
             for (T task : data) {
                 bf.write(task.toString() + System.lineSeparator());
             }
         } catch (IOException e) {
-            System.out.println("Исключение");
+            throw new ManagerSaveException("Something went wrong while trying to save the file.");
         }
     }
 }
